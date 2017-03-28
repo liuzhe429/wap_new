f8ea90c831e51c6b22eecadf5b19d327bf33db3a
liuzhe <543270835@qq.com>
2017-03-28 19:18:42 +0800

    插件目录

---
+++ components/index.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Administrator on 2017/3/28.
+ */
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Router, Route, hashHistory } from 'react-router';
+import Button from './src/Button/Button.js';
+
+let root = document.getElementById('app');
+ReactDOM.render( <Button />, root );
---
+++ components/src/Button/Button.js
@@ -0,0 +1,21 @@
+/**
+ * Created by Administrator on 2017/3/28.
+ */
+import React, { Component } from 'react';
+
+class Button extends Component {
+  handleClick(){
+    alert('戳我干嘛！');
+  }
+  render(){
+    const style = require('./Button.less');
+
+    return (
+      <button className="my-button" onClick={this.handleClick.bind(this)}>
+        快戳我
+      </button>
+    );
+  }
+}
+
+export default Button;
